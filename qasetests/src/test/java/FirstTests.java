d699f2cb271420892b6bfb9cc07ef4441e7ea201
RickSanchez5 <foreducation1987@gmail.com>
2023-02-07 18:08:49 +0300

    FirstAutoTests

---
+++ qasetests/src/test/java/FirstTests.java
@@ -0,0 +1,82 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.fail;
+
+
+public class FirstTests {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+    private JavascriptExecutor js;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.chrome.driver", "");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        js = (JavascriptExecutor) driver;
+    }
+
+    @Test
+    public void testUntitledTestCase() throws Exception {
+        driver.get("https://app.qase.io/login");
+        driver.findElement(By.id("inputEmail")).click();
+        driver.findElement(By.id("inputEmail")).clear();
+        driver.findElement(By.id("inputEmail")).sendKeys("forEducationBelov@yandex.ru");
+        driver.findElement(By.id("inputPassword")).click();
+        driver.findElement(By.id("inputPassword")).clear();
+        driver.findElement(By.id("inputPassword")).sendKeys("ViVaLaFreedom");
+        driver.findElement(By.id("btnLogin")).click();
+        driver.get("https://app.qase.io/projects");
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
+
